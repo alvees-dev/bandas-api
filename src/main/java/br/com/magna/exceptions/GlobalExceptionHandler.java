e5c531a09943193628c862c34bbec1f4317eddea
Gabriel Alves <alvesg.dev@gmail.com>
2024-09-20 16:17:16 -0300

    bad request return modification

--- src/main/java/br/com/magna/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/com/magna/exceptions/GlobalExceptionHandler.java
@@ -2,40 +2,39 @@ package br.com.magna.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import br.com.magna.dto.JsonResponse;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-	
+
 	@ExceptionHandler(PessoaNotFoundException.class)
 	public ResponseEntity<Object> handlePessoaNotFoundException(PessoaNotFoundException pnfe) {
 		String erro = pnfe.getMessage();
-		
+
 		return new ResponseEntity<>(new JsonResponse(erro), HttpStatus.NOT_FOUND);
 	}
-	
-	@ExceptionHandler(PessoaBadRequestException.class)
-	public ResponseEntity<Object> handlePessoaBadRequestException(PessoaBadRequestException pbre) {
-		String erro = pbre.getMessage();
-		
-		return new ResponseEntity<>(new JsonResponse(erro), HttpStatus.BAD_REQUEST);
-	}
-	
+
 	@ExceptionHandler(BandaNotFoundException.class)
 	public ResponseEntity<Object> handleBandaNotFoundException(BandaNotFoundException bnfe) {
 		String erro = bnfe.getMessage();
-		
+
 		return new ResponseEntity<>(new JsonResponse(erro), HttpStatus.NOT_FOUND);
 	}
-	
-	@ExceptionHandler(BandaBadRequestException.class)
-	public ResponseEntity<Object> handleBandaBadRequestException(BandaBadRequestException bbre) {
-		String erro = bbre.getMessage();
-		
-		return new ResponseEntity<>(new JsonResponse(erro), HttpStatus.BAD_REQUEST);
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Object> tratarErro400(MethodArgumentNotValidException ex) {
+		var erros = ex.getFieldErrors();
+		return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
 	}
 
+	private record DadosErroValidacao(String campo, String mensagem) {
+		public DadosErroValidacao(FieldError erro) {
+			this(erro.getField(), erro.getDefaultMessage());
+		}
+	}
 }
