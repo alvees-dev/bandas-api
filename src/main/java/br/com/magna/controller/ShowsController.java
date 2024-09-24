3b03648dc1779d80e99d7b35052a3d79f2857dd6
Gabriel Alves <alvesg.dev@gmail.com>
2024-09-24 17:15:57 -0300

    create, update and delete mapping for show

--- src/main/java/br/com/magna/controller/ShowsController.java
+++ src/main/java/br/com/magna/controller/ShowsController.java
@@ -1,5 +1,54 @@
 package br.com.magna.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.magna.dto.JsonResponse;
+import br.com.magna.dto.shows.ShowsAgendaDTO;
+import br.com.magna.dto.shows.ShowsAtualizacaoDTO;
+import br.com.magna.dto.shows.ShowsRetornoDTO;
+import br.com.magna.service.ShowsService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("shows")
 public class ShowsController {
+
+	private final ShowsService service;
+
+	public ShowsController(ShowsService service) {
+		this.service = service;
+	}
+
+	@PostMapping
+	@Transactional
+	public ResponseEntity<ShowsRetornoDTO> Show(@RequestBody @Valid ShowsAgendaDTO dados) {
+
+		var agendarShow = service.agendarShow(dados);
+		return ResponseEntity.status(HttpStatus.CREATED).body(agendarShow);
+	}
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<JsonResponse> deleteShow(@PathVariable Long id) {
+		service.deleteShows(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping("/atualizarshows/{id}")
+	@Transactional
+	public ResponseEntity<ShowsRetornoDTO> updateShow(@PathVariable Long id, @RequestBody @Valid ShowsAtualizacaoDTO dados) {
+		var shows = service.updateShow(id, dados);
+		return ResponseEntity.status(HttpStatus.OK).body(shows);
+	}
 
 }
--- src/main/java/br/com/magna/service/ShowsService.java
+++ src/main/java/br/com/magna/service/ShowsService.java
@@ -1,5 +1,45 @@
 package br.com.magna.service;
 
-public class ShowsService {
+import org.springframework.stereotype.Service;
 
+import br.com.magna.dto.JsonResponse;
+import br.com.magna.dto.shows.ShowsAgendaDTO;
+import br.com.magna.dto.shows.ShowsAtualizacaoDTO;
+import br.com.magna.dto.shows.ShowsRetornoDTO;
+import br.com.magna.exceptions.ShowNotFoundException;
+import br.com.magna.model.Shows;
+import br.com.magna.repository.ShowsRepository;
+
+@Service
+public class ShowsService {
+	
+	private final Shows shows;
+	private final ShowsRepository repository;
+
+	public ShowsService(ShowsRepository repository) {
+		this.shows = new Shows();
+		this.repository = repository;
+	}
+	
+	public ShowsRetornoDTO agendarShow(ShowsAgendaDTO dados) {
+		Shows shows = new Shows(dados);
+		repository.save(shows);
+		return new ShowsRetornoDTO(shows);
+	}
+	
+	public JsonResponse deleteShows(Long id) {
+		repository.deleteById(id);
+		return new JsonResponse("O Show com a Id" + id + "foi deletado com sucesso");
+	}
+
+	public ShowsRetornoDTO updateShow(Long id, ShowsAtualizacaoDTO dados) {
+		
+		var show = repository.findById(id)
+				.orElseThrow(() -> new ShowNotFoundException("Não foi encontrado Show com a Id digitada"));
+		shows.atualizacaoShows(dados);
+		
+		return new ShowsRetornoDTO(shows);
+	}
+	
+	
 }
